4698192ae6963202f029320d245f83480157aaac
ItzDaSrijan <97895195+ItzDaSrijan@users.noreply.github.com>
2024-11-04 11:07:15 +0530

    Add files via upload

--- Password Game: Apocalypse/app.py
+++ Password Game: Apocalypse/app.py
@@ -1,10 +1,11 @@
 # checke
-# use CTkMessageBox ig - go thru license for all libraries
 # save and load system stuff
-# delete user - simply quit on unsaved and delete and quit on saved
 # add actual rules and code variables shit
-# add open source link
-# performance check
+# performance check, make all rules frame same size
+# add more parametrized random rules and check working
+# add warning for rules disappearing, add pw length
+# save rulesdisplayed and display them ig
+# nuitka
 
 # Importing
 import customtkinter as ctk
@@ -44,7 +45,7 @@ def name(text, label):
                         pass
                     else:
                         break
-            gamepage(text[:12], uid)
+            gamepage(text[:12], uid, 'new')
     else:
         label.configure(text = 'This field is required!\nMax: 12 Characters', text_color='red')            
 
@@ -52,9 +53,9 @@ def dropfunc(mode):
     if mode == 'New Game':
         pass
     else:
-        gamepage(mode[8:], mode[:5])
+        gamepage(mode[8:], mode[:5], 'load')
 
-def addrules(condition, n):
+def addrules(condition, n, tup):
     if condition:
         if n-1 in completed or n in rules_displayed:
             c = rulesflist[n-1].cget("fg_color")
@@ -64,7 +65,7 @@ def addrules(condition, n):
                 if n+1 > 50:
                     pass
                 else:
-                    rulelabel(n+1)
+                    rulelabel(n+1, tup)
                     rules_displayed.append(n+1)
             completed.add(n)
             notcompleted.discard(n)
@@ -77,74 +78,80 @@ def addrules(condition, n):
         completed.discard(n)
     satisfied.configure(text = 'Rules satisfied: '+"{:02d}".format(len(completed)-1)+'/50')
 
-def checkrules(uname, uid):
+def checkrules(uname, uid, tup):
     if len(completed) < 51:
         pw = passwordfield.get("1.0", "end-1c")
-        addrules("1" in pw, 1)
-        addrules("2" in pw, 2)
-        addrules("3" in pw, 3)
-        addrules("4" in pw, 4)
-        addrules("5" in pw, 5)
-        addrules("6" in pw, 6)
-        addrules("7" in pw, 7)
-        addrules("8" in pw, 8)
-        addrules("9" in pw, 9)
-        addrules("10" in pw, 10)
-        addrules("11" in pw, 11)
-        addrules("12" in pw, 12)
-        addrules("13" in pw, 13)
-        addrules("14" in pw, 14)
-        addrules("15" in pw, 15)
-        addrules("16" in pw, 16)
-        addrules("17" in pw, 17)
-        addrules("18" in pw, 18)
-        addrules("19" in pw, 19)
-        addrules("20" in pw, 20)
-        addrules("21" in pw, 21)
-        addrules("22" in pw, 22)
-        addrules("23" in pw, 23)
-        addrules("24" in pw, 24)
-        addrules("25" in pw, 25)
-        addrules("26" in pw, 26)
-        addrules("27" in pw, 27)
-        addrules("28" in pw, 28)
-        addrules("29" in pw, 29)
-        addrules("30" in pw, 30)
-        addrules("31" in pw, 31)
-        addrules("32" in pw, 32)
-        addrules("33" in pw, 33)
-        addrules("34" in pw, 34)
-        addrules("35" in pw, 35)
-        addrules("36" in pw, 36)
-        addrules("37" in pw, 37)
-        addrules("38" in pw, 38)
-        addrules("39" in pw, 39)
-        addrules("40" in pw, 40)
-        addrules("41" in pw, 41)
-        addrules("42" in pw, 42)
-        addrules("43" in pw, 43)
-        addrules("44" in pw, 44)
-        addrules("45" in pw, 45)
-        addrules("46" in pw, 46)
-        addrules("47" in pw, 47)
-        addrules("48" in pw, 48)
-        addrules("49" in pw, 49)
-        addrules("50" in pw, 50)
-        window.after(500, lambda: checkrules(uname, uid))
+        addrules(len(pw) > 4, 1, tup)
+        addrules(tup[0] not in pw and tup[0].upper() not in pw, 2, tup)
+        addrules(sum(int(i) for i in pw if i.isdigit()) == 35, 3, tup)
+        addrules("4" in pw, 4, tup)
+        addrules("5" in pw, 5, tup)
+        addrules("6" in pw, 6, tup)
+        addrules("7" in pw, 7, tup)
+        addrules("8" in pw, 8, tup)
+        addrules("9" in pw, 9, tup)
+        addrules("10" in pw, 10, tup)
+        addrules("11" in pw, 11, tup)
+        addrules("12" in pw, 12, tup)
+        addrules("13" in pw, 13, tup)
+        addrules("14" in pw, 14, tup)
+        addrules("15" in pw, 15, tup)
+        addrules("16" in pw, 16, tup)
+        addrules("17" in pw, 17, tup)
+        addrules("18" in pw, 18, tup)
+        addrules("19" in pw, 19, tup)
+        addrules("20" in pw, 20, tup)
+        addrules("21" in pw, 21, tup)
+        addrules("22" in pw, 22, tup)
+        addrules("23" in pw, 23, tup)
+        addrules("24" in pw, 24, tup)
+        addrules("25" in pw, 25, tup)
+        addrules("26" in pw, 26, tup)
+        addrules("27" in pw, 27, tup)
+        addrules("28" in pw, 28, tup)
+        addrules("29" in pw, 29, tup)
+        addrules("30" in pw, 30, tup)
+        addrules("31" in pw, 31, tup)
+        addrules("32" in pw, 32, tup)
+        addrules("33" in pw, 33, tup)
+        addrules("34" in pw, 34, tup)
+        addrules("35" in pw, 35, tup)
+        addrules("36" in pw, 36, tup)
+        addrules("37" in pw, 37, tup)
+        addrules("38" in pw, 38, tup)
+        addrules("39" in pw, 39, tup)
+        addrules("40" in pw, 40, tup)
+        addrules("41" in pw, 41, tup)
+        addrules("42" in pw, 42, tup)
+        addrules("43" in pw, 43, tup)
+        addrules("44" in pw, 44, tup)
+        addrules("45" in pw, 45, tup)
+        addrules("46" in pw, 46, tup)
+        addrules("47" in pw, 47, tup)
+        addrules("48" in pw, 48, tup)
+        addrules("49" in pw, 49, tup)
+        addrules("50" in pw, 50, tup)
+        window.after(500, lambda: checkrules(uname, uid, tup))
     else:
         for i in rulesflist:
             i.configure(fg_color = '#34eb37')
         gameover(uname, uid)
 
-def rulelabel(n):
+def rulelabel(n, tup):
+    if n%2==0:
+        col = 1
+        r = (n-2)//2
+    else:
+        col = 0
+        r = (n-1)//2
     rulesf = ctk.CTkFrame(rules, fg_color = 'black')
-    rulesf.grid(row = n-1, pady = 5, padx = 10)
+    rulesf.grid(row = r, column = col, pady = 5, padx = 10)
     rulesflist.append(rulesf)
-    rulelab = ctk.CTkLabel(rulesf, text = 'Rule '+str(n)+'\n'+rules_list[n-1], wraplength = 180, padx = 10, pady = 20, fg_color = '#360c6e', font = rulesfont, corner_radius = 5)
+    rulelab = ctk.CTkLabel(rulesf, text = 'Rule '+str(n)+'\n'+rules_list[n-1].format(tup[0], tup[0].upper()), wraplength = 180, padx = 10, pady = 20, fg_color = '#360c6e', font = rulesfont, corner_radius = 5, width = 200)
     rulelab.grid(row = 0, pady = 3, padx = 3)
     rules.after(25, lambda: rules._parent_canvas.yview_moveto(1.0))
 
-def savefunc(uid, uname):
+def savefunc(uid, uname, tup):
     passwordrn = passwordfield.get("1.0", "end-1c")
     found = 0
     with open('userdata.csv', 'r', newline = '') as f:
@@ -162,7 +169,7 @@ def savefunc(uid, uname):
     os.rename('temp.csv', 'userdata.csv')
     if found == 0:
         with open('userdata.csv', 'a', newline = '') as f:
-            o = [uid, uname, passwordrn]
+            o = [uid, uname, passwordrn, tup[0]]
             csvw = csv.writer(f)
             csvw.writerow(o)
     col = saved.cget('text_color')
@@ -173,10 +180,20 @@ def savefunc(uid, uname):
         tcolor = '#00ffee'
     saved.configure(text = 'Saved!', text_color = tcolor)
 
-# checke
-def deletefunc():
-    pass
-
+def deletefunc(uid):
+    with open('userdata.csv', 'r', newline = '') as f:
+        csvr = csv.reader(f)
+        with open('temp.csv', 'a', newline = '') as g:
+            csvw = csv.writer(g)
+            for i in csvr:
+                if i[0] == uid:
+                    pass
+                else:
+                    csvw.writerow(i)
+    os.remove('userdata.csv')
+    os.rename('temp.csv', 'userdata.csv')
+    window.destroy()
+    
 def gameover(uname, uid):
     passwordfield.configure(state = 'disabled', text_color = 'gray')
     savefunc(uid, uname)
@@ -190,11 +207,17 @@ def startpage():
     
     window.title('Password Game: Apocalypse - Start')
     window.iconbitmap('icon.ico')
-    window.resizable(width=False, height=False)
+    window.after(0, lambda: window.wm_state('zoomed'))
     
     window.configure(fg_color = '#4B0082')
 
-    titleframe = ctk.CTkFrame(window, fg_color = '#4B0082')
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_rowconfigure(0, weight=1)
+
+    mainframe = ctk.CTkFrame(window, fg_color = '#4B0082')
+    mainframe.grid(row = 0)
+
+    titleframe = ctk.CTkFrame(mainframe, fg_color = '#4B0082')
     titleframe.grid(row = 0, padx = 30, pady = 20)
 
     title = ctk.CTkLabel(titleframe, text = 'Password Game: Apocalypse', text_color = '#D3D3D3', font = titlefont)
@@ -203,7 +226,7 @@ def startpage():
     hell = ctk.CTkLabel(titleframe, text = 'Welcome to Hell', font = ctk.CTkFont(family = "Helvetica", size = 30))
     hell.grid(row = 1, pady = 20)
 
-    nameframe = ctk.CTkFrame(window, fg_color = '#500666', border_width = 2, border_color = 'black')
+    nameframe = ctk.CTkFrame(mainframe, fg_color = '#500666', border_width = 2, border_color = 'black')
     nameframe.grid(row = 1, padx = 20)
 
     L = ['New Game']
@@ -232,10 +255,13 @@ def startpage():
     enter = ctk.CTkButton(nameframe, text = 'Start', fg_color = '#360c6e', hover_color = '#230647', border_width = 2, border_color = 'black', font = buttonfont, command = lambda: name(namefield.get(), length))
     enter.grid(row = 4, padx = 20, pady = 20)
 
-    warning = ctk.CTkLabel(window, font = ctk.CTkFont('Helvetica', 16), text = 'This game is supposed to be very hard and challenging.\nTry your best to not rage while playing this game.')
+    warning = ctk.CTkLabel(mainframe, font = ctk.CTkFont('Helvetica', 16), text = 'This game is supposed to be very hard and challenging.\nTry your best to not rage while playing this game.')
     warning.grid(row = 2, padx = 20, pady = 20)
 
-def gamepage(username, userid):
+    opensource = ctk.CTkLabel(mainframe, text = 'https://github.com/ItzDaSrijan/Password-Game-Apocalypse')
+    opensource.grid(row = 3, padx = 20, pady = 10)
+
+def gamepage(username, userid, mode):
     global window, passwordfield, rules, rules_list, satisfied, completed, notcompleted, rules_displayed, rulesflist, saved, save
 
     window.destroy()
@@ -246,17 +272,30 @@ def gamepage(username, userid):
 
     window.title('Password Game: Apocalypse')
     window.iconbitmap('icon.ico')
-    window.resizable(width=False, height=False)
+    window.after(0, lambda: window.wm_state('zoomed'))
 
     window.configure(fg_color = '#4B0082')
 
-    passwordloaded = ''
-    with open('userdata.csv', 'r', newline = '') as f:
-        csvr = csv.reader(f)
-        for i in csvr:
-            if userid == i[0]:
-                passwordloaded = i[2]
-                break
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_rowconfigure(0, weight=1)
+
+    mainframe = ctk.CTkFrame(window, fg_color = '#4B0082')
+    mainframe.grid(row = 0)
+
+    tup = ()
+
+    if mode == 'new':
+        passwordloaded = ''
+        r1 = 'abcdefghijklmnopqrstuvwxyz'[random.randint(0, 25)]
+    else:
+        with open('userdata.csv', 'r', newline = '') as f:
+            csvr = csv.reader(f)
+            for i in csvr:
+                if userid == i[0]:
+                    passwordloaded, r1 = i[2], i[3]
+                    break
+
+    tup = (r1,)
 
     completed = {0}
     notcompleted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50}
@@ -268,23 +307,23 @@ def gamepage(username, userid):
     rules_list = rules_text.split('\n')
 
     # Title
-    titleframegp = ctk.CTkFrame(window, fg_color = '#4B0082')
+    titleframegp = ctk.CTkFrame(mainframe, fg_color = '#4B0082')
     titleframegp.grid(row = 0, padx = 30, pady = 20)
 
     titlegp = ctk.CTkLabel(titleframegp, text = 'Password Game: Apocalypse', text_color = '#D3D3D3', font = titlefont)
     titlegp.grid(row = 0)
 
     # Second Frame
-    sframe = ctk.CTkFrame(window, fg_color = '#4B0082')
+    sframe = ctk.CTkFrame(mainframe, fg_color = '#4B0082')
     sframe.grid(row = 1)
 
     # Left
-    rules = ctk.CTkScrollableFrame(sframe, label_text = 'Rules', label_fg_color = '#360c6e', label_font = ctk.CTkFont(family = "Source Sans Pro", size = 22), label_text_color = 'white', height = 300, width = 240, fg_color = '#6e078c', corner_radius = 5, border_width = 3, border_color = 'black', scrollbar_button_color = 'black', scrollbar_button_hover_color = 'black')
+    rules = ctk.CTkScrollableFrame(sframe, label_text = 'Rules', label_fg_color = '#360c6e', label_font = ctk.CTkFont(family = "Source Sans Pro", size = 22), label_text_color = 'white', height = 400, width = 480, fg_color = '#6e078c', corner_radius = 5, border_width = 3, border_color = 'black', scrollbar_button_color = 'black', scrollbar_button_hover_color = 'black')
     rules.grid(row = 0, column = 0, padx = 20, pady = 20)
 
-    rulelabel(1)
+    rulelabel(1, tup)
 
-    rules.grid_columnconfigure(0, weight=1)
+    rules.grid_columnconfigure((0, 1), weight=1)
 
     # Right
     rightframe = ctk.CTkFrame(sframe, fg_color = '#6e078c', border_width = 3, border_color = 'black')
@@ -313,15 +352,15 @@ def gamepage(username, userid):
     # scrollbar appears so weird stuff happens and white thing, maybe scroll to bottom
     passwordfield.insert('0.0', passwordloaded)
 
-    save = ctk.CTkButton(rightframe, text = 'Save Game', fg_color = '#360c6e', hover_color = '#230647', border_width = 3, border_color = 'black', font = buttonfont, command = lambda: savefunc(userid, username))
+    save = ctk.CTkButton(rightframe, text = 'Save Game', fg_color = '#360c6e', hover_color = '#230647', border_width = 3, border_color = 'black', font = buttonfont, command = lambda: savefunc(userid, username, tup))
     save.grid(row = 2, padx = 20, pady = 10)
 
-    delete = ctk.CTkButton(rightframe, text=  'Delete Game', fg_color = '#9c0808', hover_color = '#730606', border_width = 3, border_color = 'black', font = ctk.CTkFont(family = "Source Sans Pro", size = 24), command = deletefunc)
+    delete = ctk.CTkButton(rightframe, text=  'Delete Game', fg_color = '#9c0808', hover_color = '#730606', border_width = 3, border_color = 'black', font = ctk.CTkFont(family = "Source Sans Pro", size = 24), command = lambda: deletefunc(userid))
     delete.grid(row = 3, padx = 20, pady = 10)
 
     rules_file.close()
 
-    checkrules(username, userid)
+    checkrules(username, userid, tup)
 
     window.mainloop()
 
